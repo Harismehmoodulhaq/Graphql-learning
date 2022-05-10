42fd91d558a3ad1dc07fcd9d161d1c1aecd8e1e9
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2022-05-10 12:40:18 +0300

    My first push

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,41 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s@/,
+  },
+  entities: [Post],
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__prod__,
+  password: 'haris123',
+  user: 'abdullah',
+  allowGlobalContext: true,
+} as Parameters<typeof MikroORM.init>[0];
+// type x = Re<typeof MikroORM.init>
+/* 
+
+ TODO: How to login as user in postgres sell
+* psql -U postgres
+ TODO: How to create user in postgres
+* createuser <username>
+ TODO: How to create db in postgres
+* createdb <dbname>
+* alter user <username> with encrypted password '<set your password>'
+* grant all privileges on database <databasename> to <username>
+
+
+https://hevodata.com/learn/pgadmin-docker/
+*/
+
+
+/* 
+docker run -p 5050:80 \
+    -e "PGADMIN_DEFAULT_EMAIL=user@domain.com" \
+    -e "PGADMIN_DEFAULT_PASSWORD=SuperSecret" \
+    -d dpage/pgadmin4
+*/
